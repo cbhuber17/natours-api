7146be6b8087042349fcf2b85a4ef0ed5753a1ae
Colin Huber <cbhuber@gmail.com>
2023-03-29 14:38:10 -0600

    feat: error handling.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -29,11 +32,14 @@ app.use('/api/v1/users', userRouter);
 
 // Catch unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't locate ${req.originalUrl} on this server!`,
-  });
+  // Anything passed into next assumes error
+  next(
+    new AppError(`Unable to locate ${req.originalUrl} on this server!`, 404)
+  );
 });
+
+// Middleware error handling
+app.use(globalErrorHandler);
 
 // Server code done separately
 module.exports = app;
