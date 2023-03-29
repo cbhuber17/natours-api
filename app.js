e16690b33cdd020256998e5b0505e37f7a2d631e
Colin Huber <cbhuber@gmail.com>
2023-03-29 12:04:37 -0600

    feat: add error handling for unhandled routes.

--- app.js
+++ app.js
@@ -27,5 +27,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Catch unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't locate ${req.originalUrl} on this server!`,
+  });
+});
+
 // Server code done separately
 module.exports = app;
