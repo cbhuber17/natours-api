f8d07170a15aa69ddd9dfd868f72c16fdabb8a08
Colin Huber <cbhuber@gmail.com>
2023-03-29 10:23:05 -0600

    feat: data validators.

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,12 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have <= 40 characters'],
+      minlength: [10, 'A tour name must have >= 10 characters'],
+      // validate: [
+      //   validator.isAlpha,
+      //   'Tour name must only contain alpha characters',
+      // ],
     },
     slug: {
       type: String,
@@ -23,10 +30,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be 1 at a minimum'],
+      max: [5, 'Rating must be 5 at a maximum'],
     },
     ratingsQuantity: {
       type: Number,
@@ -36,7 +49,17 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      // Custom validator
+      validate: {
+        validator: function (val) {
+          // This only points to the current doc on NEW doc creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below regular price.',
+      },
+    },
     summary: {
       type: String,
       trim: true, // Remove whitespace beginning/end
