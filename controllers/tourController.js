47d4e60438fc4948ed32d19f7789047dc14c565e
Colin Huber <cbhuber@gmail.com>
2023-03-29 22:24:48 -0600

    feat: 404 error handling when no matching ID is found in DB.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Tour = require('./../Models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
@@ -37,6 +38,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -66,6 +72,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true, // New updated document is the one returned
   });
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   // ID checking done in middleware
   res.status(200).json({
     status: 'success',
@@ -78,7 +88,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 // ------------------------------------------------------------------
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
