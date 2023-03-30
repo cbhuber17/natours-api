74557e3526394e29659c72e44f6d2754783ec282
Colin Huber <cbhuber@gmail.com>
2023-03-30 16:36:07 -0600

    feat: User model.

---
+++ Models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'An email is required'],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Incorrect email format'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    minlength: 8,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
