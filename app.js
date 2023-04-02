480cfe7123151a26b1ef4b14991327cb32323bac
Colin Huber <cbhuber@gmail.com>
2023-04-01 18:42:42 -0600

    feat: rate limiting from a certain IP; DOS and brute-force attacks.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // Shows GET, POST, status codes etc. on server console
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in 1 hour.',
+});
+
+// Limit access to the API route
+app.use('/api', limiter);
 
 app.use(express.json()); // Middleware, allows post routes
 
