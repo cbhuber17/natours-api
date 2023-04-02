f24b43a654cffccf1731247f3c2f2b98e40dbad2
Colin Huber <cbhuber@gmail.com>
2023-04-01 18:34:01 -0600

    feat: sent JWT via cookie.

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,23 @@ const signToken = (id) => {
 
 // ------------------------------------------------------------------
 
+// Send the JWT via cookie
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, // Cookie cannot be accessed or modified in any way; prevents XSS
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // only on HTTPS
+
+  res.cookie('jwt', token);
+
+  // Remove PW from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
