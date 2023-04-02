7fd388f27fe1822ec6ec55eacda57d599f22d27f
Colin Huber <cbhuber@gmail.com>
2023-04-01 18:03:45 -0600

    feat: allower user to change their profile (name and email).

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('./../Models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 // ------------------------------------------------------------------
 // Handlers
@@ -15,24 +25,66 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
+// ------------------------------------------------------------------
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if user tries to POST PW data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates.  Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // Update user document
+  // Only allow changes to name and role
+  // req.body contains everything, should not allow someone to change e.g. role to admin
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true, // Return new object
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
+    },
+  });
+});
+
+// ------------------------------------------------------------------
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
+// ------------------------------------------------------------------
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
+// ------------------------------------------------------------------
+
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
+// ------------------------------------------------------------------
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
