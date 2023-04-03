06510d8ae25932ac679e905832f2008f2d04f2dc
Colin Huber <cbhuber@gmail.com>
2023-04-03 17:57:22 -0600

    feat: Get data of current logged in user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,13 @@ const filterObj = (obj, ...allowedFields) => {
 };
 
 // ------------------------------------------------------------------
-// Handlers
+
+// Middleware
+// Get ID of currently logging in user
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 // ------------------------------------------------------------------
 
