c47d3976825e314a83ccc7bd8b269bd4c191721f
Colin Huber <cbhuber@gmail.com>
2023-04-04 10:44:19 -0600

    fix: model folders with lowercase.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const AppError = require('../utils/appError');
-const Tour = require('./../Models/tourModel');
+const Tour = require('./../models/tourModel');
 
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
