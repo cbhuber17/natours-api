99b0fc65c09cb116e05cd73265a8d15c01a3f9c8
Colin Huber <cbhuber@gmail.com>
2023-04-11 21:24:50 -0600

    env: adding compression.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const app = express();
 
@@ -68,6 +69,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 // ------------------------------------------------------------------
 
